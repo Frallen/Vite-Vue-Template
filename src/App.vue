3160786f6a9e0d55b96072f325a5b56822363780
Vladislav Popov <bladone439@gmail.com>
2023-06-19 18:56:58 +0300

    feat:init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Error v-if="isError"></Error>
+    <div v-else>
+      <Navigation></Navigation>
+      <Suspense>
+      </Suspense>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const {isError} = storeToRefs(useMain())
+
+</script>
+<style lang="less">
+
+</style>
